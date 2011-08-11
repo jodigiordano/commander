7059c3a83a753e9790a2f172436a969787667f6b
Jodi <jodi@ephemeregames.com>
2011-08-11 14:01:31 -0400

    Working mouse input!

--- Commander/Simulation/Spaceships/Steering/Manual/SpaceshipMouseMBehavior.cs
+++ Commander/Simulation/Spaceships/Steering/Manual/SpaceshipMouseMBehavior.cs
@@ -1,17 +1,20 @@
 ﻿namespace EphemereGames.Commander.Simulation
 {
+    using System;
     using Microsoft.Xna.Framework;
 
 
     class SpaceshipMouseMBehavior : SpaceshipSteeringBehavior
     {
         private float MaxRotation;
+        private float MaxMouseLengthPxSquared;
 
 
         public SpaceshipMouseMBehavior(Spaceship spaceship)
             : base(spaceship)
         {
             MaxRotation = MaxRotationPerUpdateRad;
+            MaxMouseLengthPxSquared = (float) Math.Pow(12, 2);
         }
 
 
@@ -29,10 +32,17 @@
             movement.X = MathHelper.Clamp(movement.X, -1, 1);
             movement.Y = MathHelper.Clamp(movement.Y, -1, 1);
 
+
             if (rotation != Vector3.Zero)
+            {
+                MaxRotationPerUpdateRad = (Math.Min(rotation.LengthSquared(), MaxMouseLengthPxSquared) / MaxMouseLengthPxSquared) * MaxRotation;
                 ApplyRotation(ref rotation);
+            }
             else
+            {
+                MaxRotationPerUpdateRad = MaxRotation;
                 ApplyRotation(ref movement);
+            }
 
             ApplyAcceleration(ref movement);
 
