5c3e6ee8d72a71821dd13a229da9c8aa60d287dc
Jodi <jodi@ephemeregames.com>
2011-10-25 12:29:41 -0400

    Adjusted Asteroid Belt depending on Battlefield size.

--- Commander/Simulation/PlanetarySystem/AsteroidBelt.cs
+++ Commander/Simulation/PlanetarySystem/AsteroidBelt.cs
@@ -1,5 +1,6 @@
 ﻿namespace EphemereGames.Commander.Simulation
 {
+    using System;
     using System.Collections.Generic;
     using EphemereGames.Core.Visual;
     using Microsoft.Xna.Framework;
@@ -58,6 +59,7 @@
 
             Asteroids = new List<Asteroid>(NbAsteroids);
 
+            var sizeMultiplier = Math.Max(path.X, path.Y) / (Math.Min(Preferences.BackBuffer.X, Preferences.BackBuffer.Y) * Preferences.BackBufferZoom);
 
             for (int i = 0; i < NbAsteroids; i++)
             {
@@ -65,14 +67,14 @@
                 {
                     Image = representations[Main.Random.Next(0, representations.Count)].Clone(),
                     TimeOffset = Main.Random.Next((int)(-speed/2), (int)(speed/2)),
-                    Offset = new Vector3(Main.Random.Next(-50,50),Main.Random.Next(-50,50 ),0),
+                    Offset = basePosition + new Vector3(Main.Random.Next(-50,50),Main.Random.Next(-50,50 ),0),
                     RotationSpeed = Main.Random.Next(-100, 100) / 10000.0f,
                     MovingSpeed = Main.Random.Next(1, 10)
                 };
 
                 asteroid.Image.VisualPriority = Preferences.PrioriteSimulationCeintureAsteroides;
                 asteroid.Image.Alpha = 60;
-                asteroid.Image.SizeX = (Main.Random.Next(20, 70) / 30.0f) * 3;
+                asteroid.Image.SizeX = (Main.Random.Next(20, 70) / 30.0f) * 3 * sizeMultiplier;
 
                 CelestialBody.Move(Speed, (ActualRotationTime + asteroid.TimeOffset) % Speed, ref Path, ref asteroid.Offset, ref RotationMatrix, ref asteroid.Image.position);
 
