f8d15340f121956b73b9df9c41a3b0ad1304db80
Jodi <jodi@ephemeregames.com>
2011-10-26 14:17:28 -0400

    Added "Go to website" button in Help panel.

--- Commander/Panels/HelpPanel.cs
+++ Commander/Panels/HelpPanel.cs
@@ -6,12 +6,29 @@
 
     class HelpPanel : VerticalPanel
     {
+        private PushButton VisitWebsite;
+
+
         public HelpPanel(Scene scene, Vector3 position, Vector2 size, double visualPriority, Color color)
             : base(scene, position, size, visualPriority, color)
         {
             SetTitle("How to play");
 
+            var vwText = new Text("Visit website", @"Pixelite") { SizeX = 2 };
+            VisitWebsite = new PushButton(vwText, (int) vwText.AbsoluteSize.X + 20);
+            VisitWebsite.ClickHandler = DoVisitWebsiteClicked;
+
+            AddTitleBarWidget(VisitWebsite);
+
             Alpha = 0;
+        }
+
+
+        private void DoVisitWebsiteClicked(PanelWidget widget)
+        {
+#if WINDOWS
+            System.Diagnostics.Process.Start(Preferences.WebsiteURL);
+#endif
         }
     }
 }
