50b775090f77c91eab6a8159edaaced10c1a8bc8
Jodi <jodi@ephemeregames.com>
2011-11-02 12:56:44 -0400

    Modified version. 0.1.0.0 -> 0.1.1.0

--- Commander/Properties/AssemblyInfo.cs
+++ Commander/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("09d33e20-6640-43fd-977d-48702051f49a")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
