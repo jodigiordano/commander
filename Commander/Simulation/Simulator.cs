f4c368f239d63b7c828917e8e2c633752d9d10f8
Jodi <jodi@ephemeregames.com>
2011-11-22 21:02:42 -0500

    bug fix: pause in world mode.

--- Commander/Simulation/Simulator.cs
+++ Commander/Simulation/Simulator.cs
@@ -673,7 +673,7 @@ namespace EphemereGames.Commander.Simulation
             else if (key == player.KeyboardConfiguration.Cancel)
                 SimPlayersController.DoCancelAction(player);
 
-            if (MultiverseMode)
+            if (MultiverseMode || WorldMode)
                 return;
 
             if (key == player.KeyboardConfiguration.Back && State == GameState.Running)
@@ -965,7 +965,7 @@ namespace EphemereGames.Commander.Simulation
             else if (button == player.GamepadConfiguration.AdvancedView)
                 SimPlayersController.DoAdvancedViewAction(player, true);
 
-            if (MultiverseMode)
+            if (MultiverseMode || WorldMode)
                 return;
 
             if (button == player.GamepadConfiguration.Back && State == GameState.Running)
